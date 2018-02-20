7df0b1836b51b11b4e655e4615ddf944dcc998fa
inggilove <inggilove@gmail.com>
2018-02-21 00:01:50 +0900

    Hystrix / CircuitBreaker 추가
    Swagger 추가

--- src/main/java/com/example/demo/DemoApigwApplication.java
+++ src/main/java/com/example/demo/DemoApigwApplication.java
@@ -1,16 +1,38 @@
 package com.example.demo;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 import org.springframework.context.annotation.Bean;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
 
 import com.example.demo.filters.pre.SimpleFilter;
+import com.example.demo.service.SpeechService;
 
+@EnableCircuitBreaker
 @EnableZuulProxy
+@RestController
 @SpringBootApplication
 public class DemoApigwApplication {
 
+	@Autowired
+	private SpeechService speechService;
+	
+	@Bean
+	public RestTemplate rest(RestTemplateBuilder builder) {
+		return builder.build();
+	}
+	
+	@RequestMapping("/echo") 
+	public String sendEcho() {
+		return speechService.echo();
+	}
+	
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApigwApplication.class, args);
 	}
---
+++ src/main/java/com/example/demo/service/SpeechService.java
@@ -0,0 +1,28 @@
+package com.example.demo.service;
+
+import java.net.URI;
+
+import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
+
+@Service
+public class SpeechService {
+	private final RestTemplate restTemplate;
+	
+	public SpeechService(RestTemplate rest) {
+		this.restTemplate = rest;
+	}
+	
+	@HystrixCommand(fallbackMethod = "reliableEcho")
+	public String echo() {
+		URI uri = URI.create("http://35.200.40.38:3000/api/echo");
+		
+		return this.restTemplate.getForObject(uri, String.class);
+	}
+	
+	public String reliableEcho() {
+		return "{result: 200, msg: 'echo on API Gateway'}";
+	}
+}
