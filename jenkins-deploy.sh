bb3be2bd6d52e3d5b57755e812ab0512c19b308d
inggilove <30827218+inggilove@users.noreply.github.com>
2018-02-21 18:18:31 +0900

    Update jenkins-deploy.sh
    Update real-world war

--- jenkins-deploy.sh
+++ jenkins-deploy.sh
@@ -1,39 +1,27 @@
 #!/bin/bash
 # bash Call
-# sudo bash {PATH}/deploy.sh 8080 spring-boot-jenkins
+# sudo bash {PATH}/deploy.sh Build-demo-apigw-0.0.1-SNAPSHOT.war
  
 # Field
  
-# Server Port
-# Ex) 8080
-SERVER_PORT=$1
 # Service Name
-# Ex) spring-boot-jenkins
-PROJECT_NAME=$2
+# Ex) Build-demo-apigw
+PROJECT_NAME=$1
  
 PROJECT_PATH=/var/lib/jenkins/workspace/$PROJECT_NAME/build/libs
-JAR_FILE=$PROJECT_PATH/$PROJECT_NAME-0.0.1-SNAPSHOT.jar
-TMP_PATH_NAME=/tmp/$PROJECT_NAME-pid
+WAR_FILE=$PROJECT_PATH/$PROJECT_NAME
  
 # Function
 function stop(){
     sudo echo " "
-    sudo echo "Stoping process on port: $SERVER_PORT"
-    sudo fuser -n tcp -k $SERVER_PORT # tcp $serverPort에 해당하는 port를 Kill함.
- 
-    if [ -f $TMP_PATH_NAME ]; then
-        sudo rm $TMP_PATH_NAME
-    fi
- 
+    sudo echo "Stoping process on port: $WAR_FILE"
     sudo echo " "
 }
  
 function start(){
     sudo echo " "
-    sudo nohup java -jar -Dserver.port=$SERVER_PORT $JAR_FILE /tmp 2>> /dev/null >> /dev/null &
-    sudo echo "java -jar -Dserver.port=$SERVER_PORT $JAR_FILE /tmp 2>> /dev/null >> /dev/null &"
- 
-    sudo echo $! > $TMP_PATH_NAME
+    sudo nohup java -jar $WAR_FILE /tmp 2>> /dev/null >> /dev/null &
+    sudo echo "java -jar $WAR_FILE /tmp 2>> /dev/null >> /dev/null &"
     sudo echo " "
 }
  
